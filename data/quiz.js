4aa47d027329ee7507b978f1b926e1bc5f29a32e
nathan-47 <nathan.parchment47@gmail.com>
2025-03-24 17:05:23 +0000

    quiz data first commit

---
+++ data/quiz.js
@@ -0,0 +1,22 @@
+const quizData = [
+    {
+        question: "How would you describe yourself as a person",
+        id: 0,
+        answerA: "Sporty",
+        idNextQuestionA: 10,
+        answerB: "Lazy",
+        idNextQuestionB: 11
+    },
+    {
+        question: "What are you?",
+        id: 10,
+        info: 'Hello, I am thing number 1'
+    },
+    {
+        question: "Who are you?",
+        id: 11,
+        info: 'Hello, I am thing number 2'
+    },
+];
+
+
