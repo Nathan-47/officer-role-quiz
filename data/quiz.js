020a36d9d913282708dbb826f21bf29315177a40
nathan-47 <nathan.parchment47@gmail.com>
2025-03-27 17:34:21 +0000

    count fixed again

--- data/quiz.js
+++ data/quiz.js
@@ -31,50 +31,44 @@ const quizData = [
   
 
   let currentQuestionIndex = 0;
-  let score = 0;
-  
+
+  // Place the values from answer into this array
+  let getValueTotal = [];
+  let sumTotal = 0;
+
+
     const displayQuestion = () => {
     const questionData = quizData[currentQuestionIndex];
     const questionElement = document.getElementById("question");
     const optionsElement = document.querySelector(".options");
-  
-    questionElement.textContent = questionData.question;
 
-    // Display options for a question
-//     quizData.forEach((question) => {
-//     question.options.forEach((option) => {
-//       console.log(`${option.value}`);
-//     });
-//   });
-  
-  
+    questionElement.textContent = questionData.question;  
+
     optionsElement.innerHTML = "";
     questionData.options.forEach(option => {
       const button = document.createElement("button");
       button.textContent = option.text;
-    //   console.log(option.text)
-    // console.log(`${option.value}`);
-    // const getValue = `${option.value}`;
-    // console.log(getValue);
-
 
       button.addEventListener("click", () => handleAnswer(option.value));
+
+    // Add the option text to the button
       optionsElement.appendChild(button);
     });
 
   }
 
+  // Handles the value/score given from the quizdata options
   const handleAnswer = (getValue) => {
-    if (getValue) {
-        getValue+
-        // score++;
-        console.log(getValue);
-    }
-    
 
+    // push the values to the total array
+    getValueTotal.push(getValue);
+    sumTotal = getValueTotal.reduce((acc, curr) => acc + curr, 0)
+    console.log(sumTotal);
+    
+    // Allows for the quiz to move to next question
     currentQuestionIndex++;
-    // console.log(currentQuestionIndex)
-  
+
+    // Finish the quiz once the amount of questions left are at 0
     if (currentQuestionIndex < quizData.length) {
       displayQuestion();
     } else {
@@ -82,16 +76,15 @@ const quizData = [
     }
   }
 
-  
 
-
-  // make into arrow function
+    // Display officer role to user
     const showResults = () => {
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = `Quiz finished! Your score is: ${score} out of ${quizData.length}`;
+    const getResult = document.getElementById("result");
+    getResult.textContent = `Quiz finished! Your score is: ${sumTotal} out of ${quizData.length}`;
+    console.log(sumTotal)
   }
   
-  // Start the quiz
+  // Init the quiz
   displayQuestion();
   
 
