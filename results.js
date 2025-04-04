58e91d9ce9717d3e0fce1a61ee16bb3d24cc5682
nathan-47 <nathan.parchment47@gmail.com>
2025-04-04 17:33:12 +0100

    results data added

---
+++ results.js
@@ -0,0 +1,37 @@
+const showResults = () => {
+    const resultElement = document.getElementById("result");
+
+    // Define roles through thresholds and a message
+    const roles = [
+        { 
+            threshold: 35, 
+            message: "Congratulations! You qualify for the President role." 
+        },
+        { 
+            threshold: 30, 
+            message: "Congratulations! You qualify for the VP Academic role." 
+        },
+        { 
+            threshold: 25, 
+            message: "Congratulations! You qualify for the VP Equity role." 
+        },
+        { 
+            threshold: 20,
+             message: "Congratulations! You qualify for the VP Community role." 
+        },
+        { 
+            threshold: 15, 
+            message: "Congratulations! You qualify for the VP Student Voice role." 
+        },
+    ];
+
+    // Find the correct role based on the sumTotal
+    const role = roles.find(r => sumTotal >= r.threshold);
+    const resultMessage = role ? `<br><strong>${role.message}</strong>` : "";
+
+    // Display the results message
+    resultElement.innerHTML = `
+      <h2>Quiz Completed!</h2>
+      <p>${resultMessage}</p>
+    `;
+};
