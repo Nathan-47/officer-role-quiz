1197110d29b82da23b93f4a8c588021ec45db20a
nathan-47 <nathan.parchment47@gmail.com>
2025-04-10 15:05:25 +0100

    removed results message

--- src/components/results.js
+++ src/components/results.js
@@ -1,9 +1,6 @@
 const showResults = () => {
     const resultEl = document.getElementById("result"); 
 
-    // insert this html into the results message
-    let addMsg = `Please re-take and look at questions carefully if you want to make sure this is the correct role for you.`
-
     // Define roles through thresholds and a message
     const roles = [
         { 
@@ -31,15 +28,11 @@ const showResults = () => {
     // Find the correct role based on the sumTotal
     const role = roles.find(r => sumTotal >= r.threshold);
     const resultMessage = role ? `<br><strong>${role.message}</strong>` : "";
-
-    
-    // Add additional information after the initial results message
-    resultEl.insertAdjacentHTML('afterend', addMsg);
     
 
     // Display the results message
     resultEl.innerHTML = `
-      <h2>Quiz Completed!</h2>
-      <p>${resultMessage}</p>
+      <h2 class="result-heading">Quiz Completed!</h2>
+      <p class="result-msg">${resultMessage}</p>
     `;
 };
