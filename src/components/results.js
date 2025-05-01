815088b77994ffe01181bc90cd1def038a5c8d32
nathan-47 <nathan.parchment47@gmail.com>
2025-05-01 16:07:35 +0100

    results image added

--- src/components/results.js
+++ src/components/results.js
@@ -28,11 +28,14 @@ const showResults = () => {
     // Find the correct role based on the sumTotal
     const role = roles.find(r => sumTotal >= r.threshold);
     const resultMessage = role ? `<br><strong>${role.message}</strong>` : "";
-    
 
+    // display results image
+    const resImg = './src/images/quiz_officer_team.png';
+    
     // Display the results message
     resultEl.innerHTML = `
       <h2 class="result-heading">Quiz Completed!</h2>
+      <img class="result-image" src="${resImg}" alt="officer team" />
       <p class="result-msg">${resultMessage}</p>
     `;
 };
