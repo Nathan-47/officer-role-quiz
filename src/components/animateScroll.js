849dd73107712010ec16a47b62f27250d6fd298a
nathan-47 <nathan.parchment47@gmail.com>
2025-09-13 14:45:39 +0100

    smoother aos function

--- src/components/animateScroll.js
+++ src/components/animateScroll.js
@@ -1,48 +1,23 @@
-// Observer only for certain mobile width
-function isMobile() {
-  return window.innerWidth >= 720;
-}
+window.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.fade');
 
-// Check for elements on page load
-document.addEventListener("DOMContentLoaded", () => {
-  const containers = document.querySelectorAll('.container');
-
-  containers.forEach((el) => {
-    const rect = el.getBoundingClientRect();
-    const inView = rect.top < window.innerHeight && rect.bottom > 0;
-
-    if (inView) {
-      el.classList.add('show');
-    }
-  });
-});
-
-
-// Observer function
-function runIntersectionObserver(options = {}) {
   const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
+    entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('show');
       } else {
         entry.target.classList.remove('show');
       }
     });
-  }, options);
+  }, { threshold: 0.1 });
 
-  const hiddenEls = document.querySelectorAll('.container');
-  hiddenEls.forEach((el) => observer.observe(el));
-}
+  elements.forEach(el => observer.observe(el));
 
-
-// Run observer for mobile if not meet width size, then desktop
-if (isMobile()) {
-  // Use rootMargin to trigger earlier on mobile
-  runIntersectionObserver({ rootMargin: '0px 0px -20% 0px' });
-} else {
-  const hiddenEls = document.querySelectorAll('.container');
-  hiddenEls.forEach((el) => el.classList.add('show'));
-}
-
-
-
+// Show the elements in an instant once page has loaded
+  elements.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+      el.classList.add('show');
+    }
+  });
+});
